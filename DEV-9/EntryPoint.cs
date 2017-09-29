7b58112564362caa790de171dd348b04cc2af0e3
KlyuchevenkoEkaterina <ktrin1996@mail.ru>
2017-09-29 19:00:13 +0300

    Project was added

---
+++ DEV-9/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace RandomChangeOfaConsecutiveGroupOfSymbols
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            string FirstLine = "12345678";
+            string SecondLine = "00000000";
+            LineChange lineChange = new LineChange(FirstLine, SecondLine);
+            Console.WriteLine(FirstLine + "\n" + SecondLine);
+            string resultLine = lineChange.ReplacementSubstring();
+            Console.WriteLine(resultLine);
+            Console.ReadKey();           
+
+        }
+    }
+}
---
+++ DEV-9/LineChange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace RandomChangeOfaConsecutiveGroupOfSymbols
+{
+    class LineChange
+    {      
+        public string firstLine { get; set; }
+        public string secondLine { get; set; }
+    
+
+        public LineChange(string FirstLine, string SecondLine)
+        {
+            firstLine = FirstLine;
+            secondLine = SecondLine;
+        }
+
+        Random rand = new Random();
+        //The method randomly selects a sequential group of symbols in one line, 
+        //and replaces with another group of characters of the same length.
+        public string ReplacementSubstring()
+        {
+            int indexSubstringBegin = rand.Next(0, firstLine.Length);
+            int lengthSubstring = rand.Next(1, firstLine.Length - indexSubstringBegin);
+            int indexInsertSubstring = rand.Next(0, secondLine.Length - lengthSubstring);           
+
+            string substring = firstLine.Substring(indexSubstringBegin, lengthSubstring);
+            string resultLine = secondLine.Remove(indexInsertSubstring, lengthSubstring);
+            resultLine = resultLine.Insert(indexInsertSubstring, substring);
+
+            return resultLine;
+        }
+    }
+}
